4c4c3d68add8df24f49f87d1287381c855cc5b90
liamtsao0607 <liamtsao6666@gmail.com>
2022-04-20 12:01:47 -0700

    project init

---
+++ src/main/java/com/pcsolution/demo/PcSolutionApiApplication.java
@@ -0,0 +1,13 @@
+package com.pcsolution.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcSolutionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcSolutionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcsolution/demo/PcSolutionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcsolution.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcSolutionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
